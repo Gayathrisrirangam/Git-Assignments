31bb99d92f0d3e659d9efe92056d312147a51b90
gayathri <gayatrisri2002@gmail.com>
2022-10-10 23:14:51 +0530

    changing of javascript file and commiting

--- assjavas1.js
+++ assjavas1.js
@@ -2,3 +2,5 @@
 // JavaScript source code
 // the hello world program
 console.log('Hello World');
+// the hello world program
+alert("The alert() method displays an alert box over the current window with the specified message.");
